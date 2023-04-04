e247042fa5d50db875e5d771bad9c0088c410f74
조원형/보안플랫폼팀(YA) <wonhyeong.cho@yanolja.com>
2023-04-04 09:46:04 +0900

    FaS 알고리즘 스터디 11주차 커밋

---
+++ src/FreeButSteedy/Week_11_2.java
@@ -0,0 +1,65 @@
+package FreeButSteedy;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Week_11_2 {
+
+    public static void main(String[] args) {
+
+//        int[] order = new int[]{4, 3, 1, 2, 5};
+//        int[] order = new int[]{5, 4, 3, 2, 1};
+        int[] order = new int[]{1, 2, 3, 4, 5};
+
+        System.out.println(solution(order));
+    }
+
+    static int solution(int[] order) {
+        int answer = 0;
+
+        Stack<Integer> tmp = new Stack<>();
+
+        int targetIndex = 0;
+        int target = order[targetIndex];
+
+        int index = 1;
+
+        while (index <= order.length + 1) {
+
+            System.out.println("Target: " + target);
+            System.out.println(Arrays.toString(tmp.toArray()));
+
+            if(!isPossible(target, index, order, tmp)) {
+                tmp.push(index);
+                index++;
+            }
+            else {
+                int top = tmp.pop();
+
+                if(top != target) {
+                    index++;
+                    tmp.push(top);
+                }
+
+                answer++;
+
+                targetIndex += 1;
+
+                if(targetIndex == order.length) break;
+
+                target = order[targetIndex];
+            }
+        }
+
+        return answer;
+    }
+
+    static boolean isPossible(int target, int index, int[] order, Stack<Integer> stack) {
+
+        if(order.length > index) {
+            if(order[index] ==  target) return true;
+        }
+
+        return !stack.isEmpty() && stack.peek() == target;
+    }
+}
